059ed8714f930be907b0f7f408b574adb7677cc0
Tha Gidi <you@example.com>
2020-05-02 02:43:39 +0100

    Updates

---
+++ precache-manifest.f30333771cc2acf67eb7a51a16b7eb6e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b988440e64d6ebc70d46ce35bd3c5d7",
+    "url": "/authorquiz/index.html"
+  },
+  {
+    "revision": "d0cef3f069bea483b8a9",
+    "url": "/authorquiz/static/css/main.7d72e2d9.chunk.css"
+  },
+  {
+    "revision": "e59a47163fce26bf36ec",
+    "url": "/authorquiz/static/js/2.86cf7a11.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/authorquiz/static/js/2.86cf7a11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0cef3f069bea483b8a9",
+    "url": "/authorquiz/static/js/main.ec82c1b7.chunk.js"
+  },
+  {
+    "revision": "08a8e264b0870c1b8104",
+    "url": "/authorquiz/static/js/runtime-main.41caf4d3.js"
+  }
+]);
