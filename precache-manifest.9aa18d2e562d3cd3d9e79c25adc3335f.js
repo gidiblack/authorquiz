2257255d107e4698ca498e8f91335939f1c85a24
Tha Gidi <you@example.com>
2020-06-17 09:34:48 +0100

    Updates

--- precache-manifest.9aa18d2e562d3cd3d9e79c25adc3335f.js
+++ precache-manifest.9aa18d2e562d3cd3d9e79c25adc3335f.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5b988440e64d6ebc70d46ce35bd3c5d7",
+    "revision": "f2f0b97744b2e0d882247d9ef760bee0",
     "url": "/authorquiz/index.html"
   },
   {
-    "revision": "d0cef3f069bea483b8a9",
+    "revision": "3f9b7265e7c6a161463a",
     "url": "/authorquiz/static/css/main.7d72e2d9.chunk.css"
   },
   {
-    "revision": "e59a47163fce26bf36ec",
-    "url": "/authorquiz/static/js/2.86cf7a11.chunk.js"
+    "revision": "30a1e2481659bef4d2fb",
+    "url": "/authorquiz/static/js/2.a70ec1d9.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/authorquiz/static/js/2.86cf7a11.chunk.js.LICENSE.txt"
+    "url": "/authorquiz/static/js/2.a70ec1d9.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "d0cef3f069bea483b8a9",
-    "url": "/authorquiz/static/js/main.ec82c1b7.chunk.js"
+    "revision": "3f9b7265e7c6a161463a",
+    "url": "/authorquiz/static/js/main.a4f8e8d6.chunk.js"
   },
   {
     "revision": "08a8e264b0870c1b8104",
